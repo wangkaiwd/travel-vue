dbd7295468cf9f6f990ad5d638f8a037a67d2fbd
wangkaiwd <1162486176@qq.com>
2018-07-08 00:04:14 +0800

    删除写死的数据

--- src/views/detail/components/Banner.vue
+++ src/views/detail/components/Banner.vue
@@ -37,19 +37,7 @@ export default {
   },
   data() {
     return {
-      isShowGallery: false,
-      swiperList: [
-        {
-          id: 1,
-          imgUrl:
-            "http://img1.qunarzz.com/sight/p0/201404/23/04b92c99462687fa1ba45c1b5ba4ad77.jpg_800x800_70debc93.jpg"
-        },
-        {
-          id: 2,
-          imgUrl:
-            "http://img1.qunarzz.com/sight/p0/1709/76/7691528bc7d7ad3ca3.img.png_800x800_9ef05ee7.png"
-        }
-      ]
+      isShowGallery: false
     };
   },
   methods: {
