0c2f5811dd22931465dee83756324415703f24f3
wangkaiwd <1162486176@qq.com>
2019-05-08 10:15:53 +0800

    feat: 城市列表点击事件修复

--- src/main.js
+++ src/main.js
@@ -1,28 +1,32 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store/index'
-import 'babel-polyfill'
-import fastClick from 'fastclick'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store/index';
+import 'babel-polyfill';
+import fastClick from 'fastclick';
 // 移动端300毫秒的延迟
-fastClick.attach(document.body)
-
-import flexible from 'amfe-flexible'
+fastClick.attach(document.body);
+if (process.env.NODE_ENV === 'development') {
+  const VConsole = require('vconsole');
+  const vConsole = new VConsole();
+}
+import flexible from 'amfe-flexible';
 
 // swiper组件
-import VueAwesomeSwiper from 'vue-awesome-swiper'
-import 'swiper/dist/css/swiper.css'
+import VueAwesomeSwiper from 'vue-awesome-swiper';
+import 'swiper/dist/css/swiper.css';
 
-Vue.use(VueAwesomeSwiper)
+Vue.use(VueAwesomeSwiper);
 // 初始化样式
-import './assets/styles/reset.css'
-import './assets/styles/base.less'
+import './assets/styles/reset.css';
+import './assets/styles/base.less';
 
 // 全局组件
-import BaseIcon from './components/icon/BaseIcon.vue'
-import BaseGallary from './components/gallary/Gallary'
-import BaseFadeAnimation from './components/fadeAnimation/FadeAnimation.vue'
-Vue.prototype.$bus = new Vue()
+import BaseIcon from './components/icon/BaseIcon.vue';
+import BaseGallary from './components/gallary/Gallary';
+import BaseFadeAnimation from './components/fadeAnimation/FadeAnimation.vue';
+
+Vue.prototype.$bus = new Vue();
 
 //全局组件注册
 Vue.component('base-icon', BaseIcon);
@@ -30,10 +34,10 @@ Vue.component('base-gallary', BaseGallary);
 Vue.component('base-fade-animation', BaseFadeAnimation);
 
 //是否进行生产环境提示
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
--- src/views/city/components/List.vue
+++ src/views/city/components/List.vue
@@ -42,135 +42,136 @@
 </template>
 
 <script>
-import BScroll from "better-scroll";
+  import BScroll from 'better-scroll';
 
-export default {
-  props: {
-    cities: {
-      type: Object,
-      required: true
-    },
-    hotCities: {
-      type: Array,
-      required: true
-    },
-    position: {
-      type: String,
-      required: true
-    }
-  },
-  mounted() {
-    this.$nextTick(() => {
-      this.initScroll();
-    });
-  },
-  computed: {
-    // 获取高度数组
-    calculateHeight() {
-      let heightList = [];
-      const currentCity = document.querySelector(".city-current");
-      const hotCity = document.querySelector(".city-hot");
-      let initHeight = currentCity.clientHeight + hotCity.clientHeight;
-      const content = document.querySelectorAll(".city-content");
-      for (let i = 0; i < content.length; i++) {
-        heightList.push(initHeight);
-        initHeight += content[i].clientHeight;
+  export default {
+    props: {
+      cities: {
+        type: Object,
+        required: true
+      },
+      hotCities: {
+        type: Array,
+        required: true
+      },
+      position: {
+        type: String,
+        required: true
       }
-      return heightList;
     },
-    currentCity() {
-      return this.$store.state.city;
-    }
-  },
-  watch: {
-    position(newVal) {
-      if (!newVal) return;
-      const element = this.$refs[newVal];
-      this.scroll.scrollToElement(element[0], 200, 0, 0);
-    }
-  },
-  methods: {
-    // 初始化better-scroll
-    initScroll() {
-      this.scroll = new BScroll(this.$refs.wrapper, {
-        // 必须传入该属性才能进行better-scroll滚动事件的监听，可以查看文档
-        probeType: 3
+    mounted () {
+      this.$nextTick(() => {
+        this.initScroll();
       });
-      this.scroll.on("scroll", this.onScroll);
     },
-    // 监听better-scroll的scroll事件
-    onScroll(pos) {
-      if (this.timer) {
-        clearTimeout(this.timer);
-      }
-      this.timer = setTimeout(() => {
-        let y = -pos.y;
-        for (let i = 0; i < this.calculateHeight.length; i++) {
-          // 这里只是进行估值计算，否则可能导致页面滚动和右侧字母激活不一致
-          if (y < Math.floor(this.calculateHeight[i] - 3)) {
-            this.$emit("letterScroll", i - 2);
-            return;
-          } else if (
-            y >
-            this.calculateHeight[this.calculateHeight.length - 1] - 3
-          ) {
-            this.$emit("letterScroll", i - 1);
-          }
+    computed: {
+      // 获取高度数组
+      calculateHeight () {
+        let heightList = [];
+        const currentCity = document.querySelector('.city-current');
+        const hotCity = document.querySelector('.city-hot');
+        let initHeight = currentCity.clientHeight + hotCity.clientHeight;
+        const content = document.querySelectorAll('.city-content');
+        for (let i = 0; i < content.length; i++) {
+          heightList.push(initHeight);
+          initHeight += content[i].clientHeight;
         }
-      }, 16);
+        return heightList;
+      },
+      currentCity () {
+        return this.$store.state.city;
+      }
     },
-    handleCityClick(city) {
-      this.$store.commit("changeCity", city);
-      this.$router.push("/home");
+    watch: {
+      position (newVal) {
+        if (!newVal) return;
+        const element = this.$refs[newVal];
+        this.scroll.scrollToElement(element[0], 200, 0, 0);
+      }
+    },
+    methods: {
+      // 初始化better-scroll
+      initScroll () {
+        this.scroll = new BScroll(this.$refs.wrapper, {
+          // 必须传入该属性才能进行better-scroll滚动事件的监听，可以查看文档
+          probeType: 3,
+          click: true // better-scroll默认会阻止浏览器的原生click
+        });
+        this.scroll.on('scroll', this.onScroll);
+      },
+      // 监听better-scroll的scroll事件
+      onScroll (pos) {
+        if (this.timer) {
+          clearTimeout(this.timer);
+        }
+        this.timer = setTimeout(() => {
+          let y = -pos.y;
+          for (let i = 0; i < this.calculateHeight.length; i++) {
+            // 这里只是进行估值计算，否则可能导致页面滚动和右侧字母激活不一致
+            if (y < Math.floor(this.calculateHeight[i] - 3)) {
+              this.$emit('letterScroll', i - 2);
+              return;
+            } else if (
+              y >
+              this.calculateHeight[this.calculateHeight.length - 1] - 3
+            ) {
+              this.$emit('letterScroll', i - 1);
+            }
+          }
+        }, 16);
+      },
+      handleCityClick (city) {
+        this.$store.commit('changeCity', city);
+        this.$router.push('/home');
+      }
     }
-  }
-};
+  };
 </script>
 
 <style lang="less" scoped>
-@import "~styles/varibles.less";
+  @import "~styles/varibles.less";
 
-.city-list {
-  position: absolute;
-  top: 2.1067rem;
-  left: 0;
-  bottom: 0;
-  color: #333;
-  overflow: hidden;
-  // 粘性定位的所有父元素都不能设置overflow: hidden,否则不会生效
-  // .city-content {
-  //   position: relative;
-  //   & > .city-title {
-  //     position: sticky;
-  //     top: 0;
-  //   }
-  // }
-  .city-title {
-    height: 0.6933rem;
-    background-color: #eee;
-    color: #666;
-    line-height: 0.6933rem;
-    padding-left: 0.2133rem;
-    font-size: 13px;
+  .city-list {
+    position: absolute;
+    top: 2.1067rem;
+    left: 0;
+    bottom: 0;
+    color: #333;
+    overflow: hidden;
+    // 粘性定位的所有父元素都不能设置overflow: hidden,否则不会生效
+    // .city-content {
+    //   position: relative;
+    //   & > .city-title {
+    //     position: sticky;
+    //     top: 0;
+    //   }
+    // }
+    .city-title {
+      height: 0.6933rem;
+      background-color: #eee;
+      color: #666;
+      line-height: 0.6933rem;
+      padding-left: 0.2133rem;
+      font-size: 13px;
+    }
+    .city-box {
+      display: flex;
+      flex-wrap: wrap;
+      padding: 0.16rem 0.9067rem 0.16rem 0.16rem;
+    }
+    p {
+      width: 2.7467rem;
+      padding: 0.08rem 0;
+      border: 1px solid #ccc;
+      text-align: center;
+      border-radius: 0.1067rem;
+      margin: 0.1067rem;
+    }
+    .city-content-list {
+      padding-left: 0.2133rem;
+      height: 0.9867rem;
+      line-height: 0.9867rem;
+      border-bottom: 1px solid #eaeaea;
+    }
   }
-  .city-box {
-    display: flex;
-    flex-wrap: wrap;
-    padding: 0.16rem 0.9067rem 0.16rem 0.16rem;
-  }
-  p {
-    width: 2.7467rem;
-    padding: 0.08rem 0;
-    border: 1px solid #ccc;
-    text-align: center;
-    border-radius: 0.1067rem;
-    margin: 0.1067rem;
-  }
-  .city-content-list {
-    padding-left: 0.2133rem;
-    height: 0.9867rem;
-    line-height: 0.9867rem;
-    border-bottom: 1px solid #eaeaea;
-  }
-}
 </style>
